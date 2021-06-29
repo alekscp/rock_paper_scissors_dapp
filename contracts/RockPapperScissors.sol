f3d8c759f32c0bbfff48fc6a15848d3cc6c34b19
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-29 12:05:43 +0200

    Setup game

---
+++ contracts/RockPapperScissors.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.6;
+
+contract RockPapperScissors {
+    enum State {
+        Created,
+        Joined,
+        Commited,
+        Revealed
+    }
+
+    struct Game {
+        uint id;
+        uint bet;
+        address payable[] players;
+        State state;
+    }
+    mapping(uint => Game) public games;
+    uint public gameId;
+
+    function createGame(address payable contestant) external payable {
+        require(msg.value > 0, "You have to send some ether");
+
+        address payable[] memory players = new address payable[](2);
+
+        players[0] = payable(msg.sender);
+        players[1] = contestant;
+
+        games[gameId].id = gameId;
+        games[gameId].bet = msg.value;
+        games[gameId].players = players;
+        games[gameId].state = State.Created;
+
+        gameId++;
+    }
+}
