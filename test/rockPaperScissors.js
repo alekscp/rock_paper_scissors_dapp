7a7436e60f718642b185578118cb8e7de1cadfb4
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-29 12:44:03 +0200

    Test reverts

--- test/rockPaperScissors.js
+++ test/rockPaperScissors.js
@@ -1,5 +1,7 @@
 const RockPaperScissors = artifacts.require("RockPaperScissors");
 
+const { expectRevert } = require("@openzeppelin/test-helpers");
+
 contract("RockPaperScissors", (accounts) => {
   let contract;
   const [player1, player2] = accounts;
@@ -7,15 +9,22 @@ contract("RockPaperScissors", (accounts) => {
   describe("createGame", () => {
     beforeEach(async () => {
       contract = await RockPaperScissors.new();
-    })
+    });
 
-    it("should create a game", async () => {
+    it("Creates a game", async () => {
       await contract.createGame(player1, { from: player1, value: 100 });
       const game = await contract.games(0);
 
       assert.equal(game.id.toNumber(), 0);
       assert.equal(game.bet.toNumber(), 100);
       assert.equal(game.state.toNumber(), 0);
-    })
-  })
+    });
+
+    it("Fails when called without a value", async () => {
+      await expectRevert(
+        contract.createGame(player1, { from: player1 }),
+        "You have to send some ether"
+      );
+    });
+  });
 });
