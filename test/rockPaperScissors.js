2bd2a96610d8b4c6dd65b8a2e792ab586acd0051
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-29 12:32:02 +0200

    Test create game

--- test/rockPaperScissors.js
+++ test/rockPaperScissors.js
@@ -1,7 +1,21 @@
 const RockPaperScissors = artifacts.require("RockPaperScissors");
 
-const { accounts } = require("@openzeppelin/test-environment");
+contract("RockPaperScissors", (accounts) => {
+  let contract;
+  const [player1, player2] = accounts;
 
-contract("RockPaperScissors", () => {
-  
+  describe("createGame", () => {
+    beforeEach(async () => {
+      contract = await RockPaperScissors.new();
+    })
+
+    it("should create a game", async () => {
+      await contract.createGame(player1, { from: player1, value: 100 });
+      const game = await contract.games(0);
+
+      assert.equal(game.id.toNumber(), 0);
+      assert.equal(game.bet.toNumber(), 100);
+      assert.equal(game.state.toNumber(), 0);
+    })
+  })
 });
