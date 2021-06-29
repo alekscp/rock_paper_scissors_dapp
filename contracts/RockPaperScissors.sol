8b61b603916120e61dbef2e65b126b224e3b114c
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-29 15:53:14 +0200

    Join game function

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -34,4 +34,18 @@ contract RockPaperScissors {
 
         gameId++;
     }
+
+    function joinGame(uint _gameId) external payable {
+        Game storage g = games[_gameId];
+
+        require(msg.sender == g.players[1], "Sender must be second player");
+        require(msg.value >= g.bet, "More ether needs to be sent in order to join");
+        require(g.state == State.Created, "Game must be in Created state");
+
+        if (msg.value > g.bet) {
+          payable(msg.sender).transfer(msg.value - g.bet); // Sender only pays bet value, refund difference otherwise
+        }
+
+        g.state = State.Joined;
+    }
 }
--- test/rockPaperScissors.js
+++ test/rockPaperScissors.js
@@ -6,25 +6,43 @@ contract("RockPaperScissors", (accounts) => {
   let contract;
   const [player1, player2] = accounts;
 
-  describe("createGame", () => {
-    beforeEach(async () => {
-      contract = await RockPaperScissors.new();
-    });
+  const stateMappings = {
+    created: 0,
+    joined: 1,
+    commited: 2,
+    revealed: 3
+  }
 
+  beforeEach(async () => {
+    contract = await RockPaperScissors.new();
+  });
+
+  describe("createGame", () => {
     it("Creates a game", async () => {
-      await contract.createGame(player1, { from: player1, value: 100 });
+      await contract.createGame(player2, { from: player1, value: 100 });
       const game = await contract.games(0);
 
       assert.equal(game.id.toNumber(), 0);
       assert.equal(game.bet.toNumber(), 100);
-      assert.equal(game.state.toNumber(), 0);
+      assert.equal(game.state.toNumber(), stateMappings.created);
     });
 
     it("Fails when called without a value", async () => {
       await expectRevert(
-        contract.createGame(player1, { from: player1 }),
+        contract.createGame(player2, { from: player1 }),
         "You have to send some ether"
       );
     });
   });
+
+  describe("joinGame", () => {
+    it("Joins sender to game as player 2", async () => {
+      await contract.createGame(player2, { from: player1, value: 100 });
+      await contract.joinGame(0, { from: player2, value: 100 });
+
+      const game = await contract.games(0);
+
+      assert.equal(game.state.toNumber(), stateMappings.joined)
+    })
+  })
 });
