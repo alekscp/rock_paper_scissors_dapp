4a50bf51e6b0a46779d664917a2af93174d37f83
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-30 13:09:27 +0200

    Load bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrat/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
