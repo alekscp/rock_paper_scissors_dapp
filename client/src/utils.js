3b8ae8b55bba97a3c3c18ae5c822bec9ae0ea610
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-30 13:05:25 +0200

    Fetch web3 provider

---
+++ client/src/utils.js
@@ -0,0 +1,21 @@
+import Web3 from "web3";
+
+const getWeb3 = async () => {
+  let web3
+
+  if (window.ethereum) {
+    await window.ethereum.send("eth_requestAccounts");
+
+    web3 = new Web3(window.ethereum);
+
+    return web3
+  }
+
+  if (Web3.givenProvider) {
+    web3 = new Web3(Web3.givenProvider)
+
+    return web3
+  }
+
+  web3 = new Web3("http://localhost:8545")
+};
