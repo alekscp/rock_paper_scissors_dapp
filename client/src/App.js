378aebd7cd18a7a60467a55edc45f61354d36db3
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-30 16:45:21 +0200

    Define states

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,25 @@ const STATES = ['Idle', 'Created', 'Joined', 'Commited', 'Revealed']
 
 function App() {
   const [web3, setWeb3] = useState()
+  const [accounts, setAccounts] = useState()
+  const [contract, setContract] = useState()
+  const [game, setGame] = useState({ state: '0' })
+  const [move, setMove] = useState()
 
   useEffect(() => {
     const initApp = async () => {
       const web3 = await getWeb3()
+      const accounts = await web3.eth.getAccounts()
+      const networkID = await web3.eth.net.getId()
+      const deployedNetwork = RockPaperScissors.networks[networkID]
+      const contract = new web3.eth.Contract(
+        RockPaperScissors.abi,
+        deployedNetwork && deployedNetwork.address
+      )
+
+      setWeb3(web3)
+      setAccounts(accounts)
+      setContract(contract)
     }
 
     initApp();
