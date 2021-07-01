9858ac2dcc10456f9eed2a6b3ef7ad09b9ae7ecb
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-07-01 10:51:53 +0200

    Tweaks

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,23 +1,23 @@
 import Web3 from "web3";
 
 const getWeb3 = async () => {
-  let web3
+  let web3;
 
   if (window.ethereum) {
-    await window.ethereum.send("eth_requestAccounts");
+    await window.ethereum.request("eth_requestAccounts");
 
     web3 = new Web3(window.ethereum);
 
-    return web3
+    return web3;
+  } else if (window.web3) {
+    web3 = new Web3(Web3.currentProvider);
+
+    return web3;
+  } else {
+    web3 = new Web3("http://localhost:8545");
+
+    return web3;
   }
-
-  if (Web3.givenProvider) {
-    web3 = new Web3(Web3.givenProvider)
-
-    return web3
-  }
-
-  web3 = new Web3("http://localhost:8545")
 };
 
-export { getWeb3 }
+export { getWeb3 };
